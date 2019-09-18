1fcafe5ea82616f658a45dc4a182351234c50fb1
Ashleigh Christopher <achristopher@MGL148.local>
2019-09-18 09:33:47 -0400

    setup inital structure for app

---
+++ src/ColorBlock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ColorBlock.css';
+
+class ColorBlock extends Componenet {
+  render(){
+    return (
+      <div className="App">
+      <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default ColorBlock;
---
+++ src/ColorContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ColorContainer.css';
+
+class ColorContainer extends Componenet {
+  render(){
+    return (
+      <div className="App">
+      <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default ColorContainer;
