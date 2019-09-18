153235d4d8ca13f3bdc3fbb27a6b5cebc15c78fb
Ashleigh Christopher <achristopher@MGL148.local>
2019-09-18 10:01:11 -0400

    get initial functions set up for color change

--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './ColorBlock.css';
 
 class ColorBlock extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(evt){
+    this.props.changeColor(this.props.value)
+  }
   render(){
     return (
       <div className="ColorBlock">
+        <div onClick={this.handleClick} className="ColorBlock--block">
+        </div>
       </div>
     );
   }
--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -3,10 +3,24 @@ import './ColorContainer.css';
 import ColorBlock from './ColorBlock.js';
 
 class ColorContainer extends Component {
+  static defaultProps = {
+    colorList: ['#C3E4ED', '#68838B', '#63D1F4', '#9AC0CD', '#50A6C2', '#ADD8E6', '#B2DFEE', '#00688B', '#009ACD', '#009ACD', '#0099CC', '#236B8E', '#0198E1', '#B0E2FF', '#4682B4',  '#63B8FF', '#007FFF', '#499DF5', '#104E8B'],
+    blockCount: 15
+  }
+  constructor(props){
+    super(props);
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor(evt){
+  }
   render(){
     return (
       <div className="ColorContainer">
-        <ColorBlock />
+        <ColorBlock
+          blockCount={this.props.blockCount}
+          color={this.props.colorList}
+          changeColor={this.changeColor}
+        />
       </div>
     );
   }
