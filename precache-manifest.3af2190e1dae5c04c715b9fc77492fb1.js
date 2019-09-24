247596237820ef33d9f6547fb0252fe130c1d370
Ashleigh Christopher <achristopher@mindgrub.com>
2019-09-24 15:32:24 -0400

    Updates

---
+++ precache-manifest.3af2190e1dae5c04c715b9fc77492fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65bff3ea0efd1f1d72df8a63a83a403",
+    "url": "/react-color-boxes/index.html"
+  },
+  {
+    "revision": "1e751a6dc1f62bc79d36",
+    "url": "/react-color-boxes/static/css/main.243dcdc1.chunk.css"
+  },
+  {
+    "revision": "a9d60c8b37fbcdfc0104",
+    "url": "/react-color-boxes/static/js/2.bd383059.chunk.js"
+  },
+  {
+    "revision": "1e751a6dc1f62bc79d36",
+    "url": "/react-color-boxes/static/js/main.233a56be.chunk.js"
+  },
+  {
+    "revision": "5f96693da63c92561408",
+    "url": "/react-color-boxes/static/js/runtime~main.c53b9fb3.js"
+  }
+]);
